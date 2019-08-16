058fda8d4564b6ac72705524a05999a21c6b775e
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2019-08-16 19:32:47 +0700

    remove mansory package

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -163,7 +163,6 @@
 <script src="{{url('assets/scripts/jquery.min.js')}}"></script>
 <script src="{{url('assets/scripts/bootstrap/bootstrap.min.js')}}"></script>
 <script src="{{url('assets/scripts/bootstrap/bootstrap.bundle.min.js')}}"></script>
-<script src="{{url('assets/scripts/masonry.pkgd.min.js')}}"></script>
 <script src="{{url('assets/scripts/owl.carousel.min.js')}}"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script src="{{url('assets/scripts/index.js')}}"></script>
