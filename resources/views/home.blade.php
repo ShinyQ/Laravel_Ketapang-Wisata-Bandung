1ac1ff723a7f4ba89c81176beab157b6db96577d
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2019-08-16 17:01:47 +0700

    Footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -155,7 +155,10 @@
 
     <section class="footer">
         <div class="container">
-            <p class="float-right">Copyright &copy; 2019 - Ketapang - Telkom University</p>
+          <center>
+            <img src="{{url('assets/images/logo/KetapangLogo-White.png')}}" width="150px" alt="Logo">
+             <font color="#f5f5f5" class="font-segoe text-center nopadding">&#8212; &nbsp; Copyright &copy; 2019 - Ketapang - Telkom University</p>
+          </center>
         </div>
     </section>
     <!-- End of Footer  -->
