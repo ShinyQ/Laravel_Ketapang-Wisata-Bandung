a9bcda0f337d60a0d50af3a2fc6048c9adeb3221
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2019-08-17 17:29:36 +0700

    add wisata all page WIP

--- resources/views/wisata.blade.php
+++ resources/views/wisata.blade.php
@@ -2,91 +2,61 @@
 
 @section('konten')
     <!-- Detail Hero -->
-    <section style="" class="hero-detail">
+    <!-- <section style="" class="hero-wisata">
         <div class="container d-flex flex-column-reverse align-items-start">
             <p style="color:white" class="detail-text-hl">a</p>
             <span class="detail-title">a</span>
         </div>
         <div class="hero-bg">
 
-        </div>
+        </div> -->
     </section>
     <!-- End of Detail Hero -->
 
-    <!-- Detail Tabs -->
-    <section class="hero-tabs">
-        <div class="container">
-            <ul class="nav nav-fill nav-tabs">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Deskripsi</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="detail-gallery.html">Galeri</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">QnA</a>
-                </li>
-            </ul>
-        </div>
-    </section>
-    <!-- End of Detail Tabs -->
-
-    <!-- Detail Content (Deskripsi) -->
-    <section class="deskripsi-detail">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 detail-content-desc">
-                    <h3 class="title-content">About</h3>
-                    <p class="content-text">
-                        a
-                    </p>
-                </div>
-                <div class="col-md-8 detail-content-inf">
-                    <h3 class="title-content">Information</h3>
-                    <div class="group-content-information">
-                        <h6>Nama</h6>
-                        <p class="content-text">
-                          a
-                        </p>
-                    </div>
-                    <div class="group-content-information">
-                        <h6>Alamat</h6>
-                        <p class="content-text" id="alamatWisata">
-                          a
-                        </p>
-                    </div>
-                    <div class="group-content-information">
-                        <h6>Jam Buka</h6>
-                        <p class="content-text">
-                          a
-                        </p>
-                    </div>
-                    <div class="group-content-information">
-                        <h6>Tanggal Didirikan</h6>
-                        <p class="content-text">
-                            a
-                        </p>
-                    </div>
-                    <div class="group-content-information">
-                        <h6>Telepon</h6>
-                        <p class="content-text">
-                            a
-                        </p>
+    <div class="container-wisata">
+        <div class="row no-gutters">
+            <div class="col-md-5">
+                <div class="list-wisata">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <a href="#">
+                        <div class="wisata-card" style="background-image: url('assets/images/wisata/farmhouse-lembang.jpg')">
+                            <div class="wisata-card-desc">
+                                <h5>Farmhouse Lembang</h5>
+                                <p>Jalan Danau Towuti G5E no 24</p>
+                            </div>
+                        </div>
+                    </a>
+                        </div>
+                        <div class="col-md-6">
+                        <a href="#">
+                        <div class="wisata-card" style="background-image: url('assets/images/wisata/farmhouse-lembang.jpg')">
+                            <div class="wisata-card-desc">
+                                <h5>Farmhouse Lembang</h5>
+                                <p>Jalan Danau Towuti G5E no 24</p>
+                            </div>
+                        </div>
+                    </a>
+                        </div>
+                        <div class="col-md-6">
+                        <a href="#">
+                        <div class="wisata-card" style="background-image: url('assets/images/wisata/farmhouse-lembang.jpg')">
+                            <div class="wisata-card-desc">
+                                <h5>Farmhouse Lembang</h5>
+                                <p>Jalan Danau Towuti G5E no 24</p>
+                            </div>
+                        </div>
+                    </a>
+                        </div>
                     </div>
                 </div>
             </div>
+            <div class="col-md-7">
+                 <div class="map-wisata " id="mapWisata">
+                 </div>
+            </div>
         </div>
-        <div class="map-detail" id="map">
-        </div>
-    </section>
-    <!-- Detail Content (Deskripsi)-->
-
-    <section class="footer">
-        <div class="container">
-            <p class="float-right">Copyright &copy; 2019 - Ketapang - Telkom University</p>
-        </div>
-    </section>
-    <!-- End of Footer  -->
+    </div>
     </body>
     <script src="{{url('assets/scripts/jquery.min.js')}}"></script>
     <script src="{{url('assets/scripts/bootstrap/bootstrap.min.js')}}"></script>
@@ -95,18 +65,20 @@
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYTqP6EacKcUYlnQaaGs2TlrKwAhUonoY&callback=initMap"
     async defer></script>
     <script>
-    // Mansory Libarary Initialization
-    $(".gallery-detail").masonry({
-      itemSelector: ".grid-item",
-      columnWidth: 350,
-      gutter: 10
-    });
 
+    // Function to determine the size of the map div
+    function resizeWindow(){
+        $('#mapWisata').css({"height":$(window).height()-$(".navbar").height()});
+    }
+    resizeWindow();
+    window.addEventListener("resize", ()=>{
+        resizeWindow();
+    });
     // AIzaSyDYTqP6EacKcUYlnQaaGs2TlrKwAhUonoY
     function initMap() {
-      const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -6.903363, lng: 107.6081381 },
-        zoom: 16
+      const map = new google.maps.Map(document.getElementById("mapWisata"), {
+        center:  {lat: -6.9009925, lng: 107.6314009},
+        zoom: 12
       });
 
       const geocoder = new google.maps.Geocoder();
@@ -115,18 +87,32 @@
     }
 
     function geoLocation(geocoder, mapResults) {
-      const address = $("#alamatWisata").text();
-      geocoder.geocode({ address: address }, function(res, status) {
+      const address = [
+        'Telkom University',
+        'Farmhouse Lembang',
+        'Bandung Digital Valley'
+      ];
+
+      address.map(location=>{
+        geocoder.geocode({ address: location }, function(res, status) {
         if (status == "OK") {
-          mapResults.setCenter(res[0].geometry.location);
+            // mapResults.setCenter(res[0].geometry.location);
           const marker = new google.maps.Marker({
             map: mapResults,
             position: res[0].geometry.location
           });
+
+          marker.addListener('click',()=>{
+              console.log('clicked')
+              mapResults.setZoom(15);
+              mapResults.setCenter(res[0].geometry.location)
+          })
         } else {
           console.log(status);
         }
       });
+      })
+      
     }
     </script>
     </html>
